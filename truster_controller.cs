5d8906e2768b671a15cf1ed9e8e3d1d0b6d72d31
Marchenko Ilya <90874301+AmateurBoy@users.noreply.github.com>
2025-05-04 02:00:47 +0300

    create base file

---
+++ truster_controller.cs
@@ -0,0 +1,14 @@
+public Program()
+{
+    Runtime.UpdateFrequency = UpdateFrequency.Update10;
+}
+
+public void Save()
+{
+    
+}
+
+public void Main(string argument, UpdateType updateSource)
+{
+    Echo("Hello from Space Engineers!");
+}
